11570153156dd165ad6c68673cab83487ac438f8
bedreski <fabianabedreski@gmail.com>
2022-04-19 14:25:06 -0300

    added grid cards and material elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
+//Angular modules
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatTabsModule} from '@angular/material/tabs';
-import { ListaDeFilmesComponent } from './components/lista-de-filmes/lista-de-filmes.component';
-import { CriarFilmeComponent } from './components/criar-filme/criar-filme.component';
-import { EditarFilmeComponent } from './components/editar-filme/editar-filme.component';
-
 import {AngularFireModule} from '@angular/fire/compat';
 import {AngularFirestoreModule} from '@angular/fire/compat/firestore';
 import {AngularFireStorageModule} from '@angular/fire/compat/storage';
 import {AngularFireAuthModule} from '@angular/fire/compat/auth';
+import {MatCardModule} from '@angular/material/card';
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListaDeFilmesComponent } from './components/lista-de-filmes/lista-de-filmes.component';
+import { CriarFilmeComponent } from './components/criar-filme/criar-filme.component';
+import { EditarFilmeComponent } from './components/editar-filme/editar-filme.component';
 import { environment } from 'src/environments/environment';
 import { LoginComponent } from './components/login/login.component';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
@@ -39,7 +41,9 @@ import { CadastroComponent } from './components/cadastro/cadastro.component';
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
     AngularFireStorageModule,
-    AngularFireAuthModule
+    AngularFireAuthModule, 
+    MatCardModule, 
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
